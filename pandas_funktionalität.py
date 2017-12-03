a973c13134be6bf68880f37a962db45e97a970cf
whapunkt <wolfgangchohlfeld@gmail.com>
2017-12-03 16:45:39 +0100

    Funktionalität mit Pandas in Separater Datei

--- pandas_funktionalität.py
+++ pandas_funktionalität.py
@@ -1,12 +1,14 @@
 # This Python file uses the following encoding: utf-8
 
-import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 # -------------------------------------------Einlesen der CSV---------------------------------------------------------------
 
 data = pd.read_csv("Inputs_WHO.csv", sep=";") # Name durch entryInput.get() auswechseln
+
+print(data.describe()) # statistische Auswertung zu den Eingelesenen Daten wird angezeigt
 
 
 # ------------------------------------------Variablendeklaration-----------------------------------------------------------
@@ -24,21 +26,34 @@ revPV = data.iat[0,3]# Einspeisevergütung in der betreffenden Stunde
 #----------------Funktionalität GUI------------
 
 def berechnenErsparnis():
+
     print("Es sind " + str(data.__len__()) + " Werte vorhanden.")
     i=0
     savings = 0
     while(i<data.__len__()):
 
-        if (((data.iat[i,1])*pvCap)>(data.iat[i,4])):
+
+        if (((data.iat[i,1])*pvCap)>(data.iat[i,4])): # falls mehr Solarstrom produziet wird, als Last anfällt, speise Strom ein, erhalte Entgelt
             print("Überkapazität zum Zeitpunkt " + str(data.iat[i,0]))
             savings = savings + (((data.iat[i,1])*pvCap)/1000)-((data.iat[i,4])*data.iat[0,3]/1000) # die savings werden um die Einspeisevergütung erhöht
+
         else:
             savings = savings + ((data.iat[i,4]/1000*data.iat[i,2]/100)-((data.iat[i,4]/1000)-(data.iat[i,1])*pvCap)/1000*data.iat[0,2]/100) # print durch textErgebnis.insert(), END auswechseln
             #                (Lastgang [W] * Stromkosten [cent/KWh]/100)- (Lastgang [W]- PV Auslastung * PV Kapazität [W])* Stromkosten [cent/KWh]/100)
             #                (                  in Euro                                                                         in Euro
         i=i+1
 
-    print("Die jährliche Ersparnis beträgt: " + str(savings/1000) + "€")
+    print("Die jährliche Ersparnis beträgt: " + str(round(savings/1000, 2)) + "€")
+
+    #-------------------Plotten der Kurven---------------------------
+    '''
+    x = np.linspace(0, 35039,35039)
+
+    plot = data.plot(data[x, "PV usage [0:1]"], kind = "line")
+    plt.show()
+    '''
+
+
     return
 
 berechnenErsparnis()
